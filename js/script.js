7cbf4cd593fa6a691de78cbfd5a4c55d992c5d2b
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2023-12-23 17:43:38 +0700

    Dah jadi Deh

--- js/script.js
+++ js/script.js
@@ -13,14 +13,41 @@ function submitHendler (){
 
     let barangValue = document.getElementById('barang').value;
     let hargaValue = document.getElementById('harga').value;
+    let namaValue = document.getElementById('nama').value;
 
     let tempObj = {
         id,
         nama: barangValue,
         harga: +hargaValue
     }
+    document.getElementById('namaCart').innerHTML = namaValue;
     carts.push(tempObj);
-    console.log(carts);
+    getCarts();
+    //console.log(carts);
 }
 
+function getCarts(){
+
+    let tBody = document.getElementById('tBody');
+    tBody.innerHTML = '';
+    carts.forEach(cart => {
+        tBody.innerHTML += `<tr>
+        <td>${cart.id}</td>
+        <td>${cart.nama}</td>
+        <td>${cart.harga}</td>
+        </tr>`
+    })
+}
+
+let BtnPrint = document.getElementById('btnPrint')
+
 BtnSubmit.addEventListener('click', submitHendler)
+
+function printHandler (){
+    // console.log('cetak')
+    document.querySelector('.form-box').style.display ='none';
+    BtnPrint.style.display='none';
+    window.print();
+}
+
+BtnPrint.addEventListener('click', printHandler);
